e2bfaaa3e59770c87f037beeb2b06c29fd5fc0e7
Jonathan Gan <jonathang132298@gmail.com>
2019-07-26 16:40:39 -0400

    Startig React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { button } from 'react';
+// import logo from './misc/logo.svg';
 import './App.css';
 
+// var Component = React.createClass({
+//   getInitialState: App () {
+//     return {
+//       inputValue: ''
+//     }
+//   },
+// updateInputValue: function(evt) {
+//   this.setState({
+//     inputValue: evt.target.value
+//   });
+// }
+
 function App() {
+
   return (
     <div className="App">
+      
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        
+        {/* <img src={logo} className="App-logo" alt="niggers" /> */}
+
+        <h1>
+          Welcoem to Prepit
+        </h1>
+        
+        <p style={{ marginTop: '20px' }} >
+          To get started, please enter how much you make.
         </p>
+
+        <input
+        style={{ marginTop: '20px' }} 
+        placeholder="Enter monthly income"
+        //value="penis"
+        />
+        <button
+        style={{ marginTop: '20px' }} 
+        title="Enter"
+        />
         <a
+          style={{marginTop: '40px'}} 
           className="App-link"
-          href="https://reactjs.org"
+          href="https://Prepit.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          
+          Visit Prepit
         </a>
       </header>
+
+    
+
     </div>
   );
 }
