f471cd5ebcc6768197c6e9647b9bfb60572d15f8
Jonathan Gan <jonathang132298@gmail.com>
2019-08-08 11:36:07 -0400

    Adding Resources

--- src/pages/Resources.js
+++ src/pages/Resources.js
@@ -7,7 +7,11 @@ function Resources() {
       <h1>
         Resources
         </h1>
-      <a href="http://www.farmersmarketonline.com/fm/Florida.htm"> Farmers Markets </a>
+      <a href="http://www.farmersmarketonline.com/fm/Florida.htm"> Farmers Markets in Florida </a> 
+      <a href="https://www.eatright.org"> Eatright.org </a>
+      <a href="https://www.choosemyplate.gov/MyPlatePlan"> Meal Planning </a>
+      <a href="https://www.sportsrd.org/wp-content/uploads/2018/11/Dietary-Supplements-Fact-Sheet.pdf"> Workout Supplement PDF </a>
+      <a href="https://www.sportsrd.org/wp-content/uploads/2018/11/Healthy-Grocery-Shopping.pdf"> Healthy Grocery Shopping PDF </a>
     </header>
   )
 };
